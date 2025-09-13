711eff9c41d4313624792f3718732b02893919e3
Emanuele <egalli64@gmail.com>
2025-09-13 11:39:45 +0200

    setup

---
+++ eureka-server/src/main/java/com/example/eureka/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+/*
+ * Spring Boot Microservices tutorial - Eureka Server
+ * 
+ * https://github.com/egalli64/spring-microservices
+ */
+package com.example.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
