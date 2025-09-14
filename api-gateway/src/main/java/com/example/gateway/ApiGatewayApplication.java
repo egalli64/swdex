81c909b591a1e2cf8ae374da74a958241787b342
Emanuele <egalli64@gmail.com>
2025-09-14 11:30:59 +0200

    setup

---
+++ api-gateway/src/main/java/com/example/gateway/ApiGatewayApplication.java
@@ -0,0 +1,18 @@
+/*
+ * Spring Boot Microservices tutorial - API Gateway
+ * 
+ * https://github.com/egalli64/spring-microservices
+ */
+package com.example.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
