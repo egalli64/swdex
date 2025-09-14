33522b364d4b02eecd9eb8edb7bba9ce51624b10
Emanuele <egalli64@gmail.com>
2025-09-14 11:17:52 +0200

    setup

---
+++ order-service/src/main/java/com/example/order/OrderServiceApplication.java
@@ -0,0 +1,18 @@
+/*
+ * Spring Boot Microservices tutorial - Order service
+ * 
+ * https://github.com/egalli64/spring-microservices
+ */
+package com.example.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
+}
