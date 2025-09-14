9b28c1cc472e95c8fb6686af67dc4ecbc7ecb1d1
Emanuele <egalli64@gmail.com>
2025-09-14 11:19:23 +0200

    entity

---
+++ order-service/src/main/java/com/example/order/Order.java
@@ -0,0 +1,89 @@
+/*
+ * Spring Boot Microservices tutorial - Order service
+ * 
+ * https://github.com/egalli64/spring-microservices
+ */
+package com.example.order;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private Long productId;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private String status;
+
+    public Order() {
+    }
+
+    public Order(Long userId, Long productId, Integer quantity, String status) {
+        this.userId = userId;
+        this.productId = productId;
+        this.quantity = quantity;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Order [id=" + id + ", userId=" + userId + ", productId=" + productId + ", quantity=" + quantity
+                + ", status=" + status + "]";
+    }
+}
