cd5403ba237463758291f106d5ca74ded68fe380
Emanuele <egalli64@gmail.com>
2025-09-16 13:49:35 +0200

    RestTemplate bean

---
+++ order-service/src/main/java/com/example/order/RestConfig.java
@@ -0,0 +1,21 @@
+/*
+ * Spring Boot Microservices tutorial - Order service
+ * 
+ * https://github.com/egalli64/spring-microservices
+ */
+package com.example.order;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestConfig {
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
