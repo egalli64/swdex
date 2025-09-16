6b5d4f8047c3f3f665ac0f86137436c1ac3c5aa0
Emanuele <egalli64@gmail.com>
2025-09-16 13:49:12 +0200

    service to service by RestTemplate

--- order-service/src/main/java/com/example/order/OrderService.java
+++ order-service/src/main/java/com/example/order/OrderService.java
@@ -11,14 +11,19 @@ import java.util.Optional;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class OrderService {
-    private static Logger log = LogManager.getLogger(OrderService.class);
-    private final OrderRepository repo;
+    private static final String USER_URI = "http://user-service/api/users/";
+    private static final Logger log = LogManager.getLogger(OrderService.class);
 
-    public OrderService(OrderRepository repo) {
+    private final OrderRepository repo;
+    private final RestTemplate rest;
+
+    public OrderService(OrderRepository repo, RestTemplate rest) {
         this.repo = repo;
+        this.rest = rest;
     }
 
     public List<Order> getAll() {
@@ -31,8 +36,19 @@ public class OrderService {
         return repo.findById(id);
     }
 
+    /**
+     * The user should exist to save an order for it
+     */
     public Order save(Order order) {
         log.traceEntry("save({})", order);
+
+        try {
+            rest.getForObject(USER_URI + order.getUserId(), Object.class);
+        } catch (Exception ex) {
+            log.warn("User not found", ex);
+            throw new UserNotFoundException(order.getUserId());
+        }
+
         return repo.save(order);
     }
 
