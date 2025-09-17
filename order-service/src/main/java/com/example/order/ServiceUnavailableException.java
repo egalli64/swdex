3f679ab7472566690a5f10fab068808a44a69aa1
Emanuele <egalli64@gmail.com>
2025-09-17 11:49:53 +0200

    custom exception

---
+++ order-service/src/main/java/com/example/order/ServiceUnavailableException.java
@@ -0,0 +1,13 @@
+/*
+ * Spring Boot Microservices tutorial - Order service
+ * 
+ * https://github.com/egalli64/spring-microservices
+ */
+package com.example.order;
+
+@SuppressWarnings("serial")
+public class ServiceUnavailableException extends RuntimeException {
+    public ServiceUnavailableException(String message) {
+        super(message);
+    }
+}
