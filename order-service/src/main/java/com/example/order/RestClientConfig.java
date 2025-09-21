9672b1b50931aa32e02ca60c34f91523a797c028
Emanuele <egalli64@gmail.com>
2025-09-21 18:03:07 +0200

    Plain vs LoadBalanced RestClient

---
+++ order-service/src/main/java/com/example/order/RestClientConfig.java
@@ -0,0 +1,36 @@
+/*
+ * Spring Boot Microservices tutorial - Order service
+ * 
+ * https://github.com/egalli64/spring-microservices
+ */
+package com.example.order;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.client.RestClient;
+
+/**
+ * Beans for RestClient.Builder 
+ */
+@Configuration
+public class RestClientConfig {
+    /**
+     * The primary option is for the plain RestClient.Builder
+     */
+    @Bean
+    @Primary
+    RestClient.Builder plainRestClientBuilder() {
+        return RestClient.builder();
+    }
+
+    /**
+     * The load balanced version requires the qualified name to be accessed
+     */
+    @Bean
+    @LoadBalanced
+    RestClient.Builder loadBalancedRestClientBuilder() {
+        return RestClient.builder();
+    }
+}
