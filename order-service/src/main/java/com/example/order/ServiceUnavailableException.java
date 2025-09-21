3319f468c07605c31dd965465134cbe8da9ecf99
Emanuele <egalli64@gmail.com>
2025-09-21 17:59:46 +0200

    Overload w/ cause

--- order-service/src/main/java/com/example/order/ServiceUnavailableException.java
+++ order-service/src/main/java/com/example/order/ServiceUnavailableException.java
@@ -10,4 +10,8 @@ public class ServiceUnavailableException extends RuntimeException {
     public ServiceUnavailableException(String message) {
         super(message);
     }
+
+    public ServiceUnavailableException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
