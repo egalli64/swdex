50359fd7c317ce14e7dc98d9c18c3947bc433297
Emanuele <egalli64@gmail.com>
2025-09-22 10:23:49 +0200

    response status 201 on creation

--- order-service/src/main/java/com/example/order/LoadBalancedController.java
+++ order-service/src/main/java/com/example/order/LoadBalancedController.java
@@ -7,9 +7,11 @@ package com.example.order;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -31,6 +33,7 @@ public class LoadBalancedController {
      * </pre>
      */
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Order create(@RequestBody Order order) {
         log.traceEntry("create({})", order);
         return svc.save(order);
--- user-service/src/main/java/com/example/user/UserController.java
+++ user-service/src/main/java/com/example/user/UserController.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -63,6 +65,7 @@ public class UserController {
      * </pre>
      */
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public User create(@RequestBody User user) {
         log.traceEntry("create({})", user);
         return svc.save(user);
