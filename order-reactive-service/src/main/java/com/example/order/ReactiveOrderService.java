4e9d02d4dfa4d464d98d8aa24bf8a6416cccd8d7
Emanuele <egalli64@gmail.com>
2025-09-24 11:14:27 +0200

    user name for an order

--- order-reactive-service/src/main/java/com/example/order/ReactiveOrderService.java
+++ order-reactive-service/src/main/java/com/example/order/ReactiveOrderService.java
@@ -9,14 +9,14 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClient.ResponseSpec;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import reactor.core.publisher.Mono;
 
 @Service
 public class ReactiveOrderService {
-    private static final String SVC_ID = "user-service";
-    private static final String SVC_URI = "/api/users/";
+    private static final String URI_USER_BY_ID = "http://user-service/api/users/{id}";
     private static final Logger log = LogManager.getLogger(ReactiveOrderService.class);
 
     private final ReactiveOrderRepository repo;
@@ -39,7 +39,7 @@ public class ReactiveOrderService {
     public Mono<ReactiveOrder> save(ReactiveOrder order) {
         log.traceEntry("save({})", order);
 
-        return client.get().uri("http://" + SVC_ID + SVC_URI + order.getUserId()).retrieve().toBodilessEntity()
+        return clientGetUser(order).toBodilessEntity() //
                 .onErrorResume(WebClientResponseException.NotFound.class, ex -> {
                     log.warn("Attempted to create order for non-existing user {}", order.getUserId());
                     return Mono.error(new UserNotFoundException(order.getUserId(), ex));
@@ -47,5 +47,33 @@ public class ReactiveOrderService {
                     log.error("Failed to invoke the user service", ex);
                     return Mono.error(new ServiceUnavailableException("User service is not available", ex));
                 }).flatMap(_ -> repo.save(order));
+    }
+
+    /**
+     * <ul>
+     * <li>Use the repository to load in memory the required order</li>
+     * <li>Flat map the order to user by WebClient</li>
+     * <li>Map (synchronous) to extract the name from the user</li>
+     * </ul>
+     */
+    public Mono<String> getUserName(Long orderId) {
+        log.traceEntry("getUserName({})", orderId);
+
+        return repo.findById(orderId) //
+                .flatMap(order -> clientGetUser(order).bodyToMono(UserDTO.class)
+                        .onErrorResume(WebClientResponseException.NotFound.class, ex -> {
+                            log.warn("Looking for non-existing user {}", order.getUserId());
+                            return Mono.error(new UserNotFoundException(order.getUserId(), ex));
+                        }).onErrorResume(WebClientResponseException.class, ex -> {
+                            log.error("Failed to invoke the user service", ex);
+                            return Mono.error(new ServiceUnavailableException("User service is not available", ex));
+                        })) //
+                .map(user -> user.name());
+    }
+
+    private ResponseSpec clientGetUser(ReactiveOrder order) {
+        log.traceEntry("clientGetUser({})", order);
+
+        return client.get().uri(URI_USER_BY_ID, order.getUserId()).retrieve();
     }
 }
