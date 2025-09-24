95bf08bd0c449bd604900be91bd0c80f10b0836b
Emanuele <egalli64@gmail.com>
2025-09-24 11:04:08 +0200

    readability

--- order-service/src/main/java/com/example/order/OrderRepository.java
+++ order-service/src/main/java/com/example/order/OrderRepository.java
@@ -6,8 +6,6 @@
 package com.example.order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 }
--- user-service/src/main/java/com/example/user/UserRepository.java
+++ user-service/src/main/java/com/example/user/UserRepository.java
@@ -6,8 +6,6 @@
 package com.example.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
