8e15b02d728503a9f4699d50740391f6c7f3803e
Emanuele <egalli64@gmail.com>
2025-09-24 11:15:35 +0200

    access get user name for order functionality

--- order-reactive-service/src/main/java/com/example/order/ReactiveOrderController.java
+++ order-reactive-service/src/main/java/com/example/order/ReactiveOrderController.java
@@ -8,6 +8,8 @@ package com.example.order;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +22,7 @@ import reactor.core.publisher.Mono;
  * Using a WebClient in asynchronous (non-blocking) way
  */
 @RestController
-@RequestMapping("/api/reactive-orders/")
+@RequestMapping("/api/reactive-orders")
 public class ReactiveOrderController {
     private static Logger log = LogManager.getLogger(ReactiveOrderController.class);
 
@@ -32,7 +34,7 @@ public class ReactiveOrderController {
 
     /**
      * <pre>
-        curl -X POST http://localhost:8080/api/reactive-orders/ ^
+        curl -X POST http://localhost:8080/api/reactive-orders ^
         -H "Content-Type: application/json" ^
         -d "{\"userId\": 1, \"productId\": 42, \"quantity\": 6, \"status\": \"pending\"}"
      * </pre>
@@ -41,6 +43,19 @@ public class ReactiveOrderController {
     @ResponseStatus(HttpStatus.CREATED)
     public Mono<ReactiveOrder> create(@RequestBody ReactiveOrder order) {
         log.traceEntry("create({})", order);
+
         return svc.save(order);
+    }
+
+    /**
+     * <pre>
+        curl -i -X GET http://localhost:8080/api/reactive-orders/123/username
+     * </pre>
+     */
+    @GetMapping("/{id}/username")
+    public Mono<String> getOrderUserName(@PathVariable Long id) {
+        log.traceEntry("getOrderUserName({})", id);
+
+        return svc.getUserName(id).switchIfEmpty(Mono.error(new OrderNotFoundException(id)));
     }
 }
