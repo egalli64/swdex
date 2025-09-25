c594ad18ed36632796f756d217b43b10895a39fa
Emanuele <egalli64@gmail.com>
2025-09-25 14:38:54 +0200

    Message for MQ

---
+++ product-service/src/main/java/com/example/product/Product.java
@@ -0,0 +1,9 @@
+/*
+ * Spring Boot Microservices tutorial - Product service
+ * 
+ * https://github.com/egalli64/spring-microservices
+ */
+package com.example.product;
+
+public record Product(String name, String description) {
+}
