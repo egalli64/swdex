0e93049b4d6fb897bb5941bb343d8460715195e4
Emanuele <egalli64@gmail.com>
2025-09-25 14:49:29 +0200

    RabbitMQ subscriber stub

---
+++ email-service/src/main/java/com/example/email/EmailServiceApplication.java
@@ -0,0 +1,16 @@
+/*
+ * Spring Boot Microservices tutorial - Email service
+ * 
+ * https://github.com/egalli64/spring-microservices
+ */
+package com.example.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EmailServiceApplication.class, args);
+    }
+}
