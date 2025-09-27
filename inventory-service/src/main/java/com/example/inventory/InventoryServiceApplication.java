dea2e777acb16100d7fc41299b00c32b55233481
Emanuele <egalli64@gmail.com>
2025-09-27 11:35:18 +0200

    stub

---
+++ inventory-service/src/main/java/com/example/inventory/InventoryServiceApplication.java
@@ -0,0 +1,16 @@
+/*
+ * Spring Boot Microservices tutorial - Inventory Service
+ * 
+ * https://github.com/egalli64/spring-microservices
+ */
+package com.example.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+}
