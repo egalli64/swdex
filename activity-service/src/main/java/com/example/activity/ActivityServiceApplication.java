7af5b77733811a8fdb2c64a5af041b9132e3d8ea
Emanuele <egalli64@gmail.com>
2025-09-29 13:48:29 +0200

    activity service stub

---
+++ activity-service/src/main/java/com/example/activity/ActivityServiceApplication.java
@@ -0,0 +1,16 @@
+/*
+ * Spring Boot Microservices tutorial - Activity Service
+ * 
+ * https://github.com/egalli64/spring-microservices
+ */
+package com.example.activity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ActivityServiceApplication.class, args);
+    }
+}
