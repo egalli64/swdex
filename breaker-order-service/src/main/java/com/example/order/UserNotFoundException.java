2f9c2bbfcf9feafdabca3311fcc95ab89198ff40
Emanuele <egalli64@gmail.com>
2025-10-15 17:26:13 +0200

    User Service support

---
+++ breaker-order-service/src/main/java/com/example/order/UserNotFoundException.java
@@ -0,0 +1,17 @@
+/*
+ * Spring Boot Microservices tutorial - Circuit Breaker Order Service
+ * 
+ * https://github.com/egalli64/spring-microservices
+ */
+package com.example.order;
+
+@SuppressWarnings("serial")
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(Long userId) {
+        super("User not found: " + userId);
+    }
+
+    public UserNotFoundException(Long userId, Throwable cause) {
+        super("User not found: " + userId, cause);
+    }
+}
