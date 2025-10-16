f44f737c6f4e9c49603942dabbca597d9e755e82
Emanuele <egalli64@gmail.com>
2025-10-16 13:17:49 +0200

    user service support

---
+++ fallback-order-service/src/main/java/com/example/order/UserDTO.java
@@ -0,0 +1,9 @@
+/*
+ * Spring Boot Microservices tutorial - Fallback Order Service
+ * 
+ * https://github.com/egalli64/spring-microservices
+ */
+package com.example.order;
+
+public record UserDTO(Long id, String name, String email) {
+}
