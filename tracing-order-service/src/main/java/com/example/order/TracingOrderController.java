e46a5be3c490005e6e7323c00c3c1f0246d0a987
Emanuele <egalli64@gmail.com>
2025-10-21 17:06:13 +0200

    a simple controller

---
+++ tracing-order-service/src/main/java/com/example/order/TracingOrderController.java
@@ -0,0 +1,44 @@
+/*
+ * Spring Boot Microservices tutorial - Tracing Order Service
+ * 
+ * https://github.com/egalli64/spring-microservices
+ */
+package com.example.order;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/tracing-orders")
+public class TracingOrderController {
+    private static Logger log = LogManager.getLogger(TracingOrderController.class);
+
+    private final TracingOrderService svc;
+
+    public TracingOrderController(TracingOrderService svc) {
+        this.svc = svc;
+    }
+
+    /**
+     * <pre>
+        curl -X GET http://localhost:8080/api/tracing-orders
+     * </pre>
+     */
+    @GetMapping()
+    public int getOrderInfo() {
+        log.traceEntry("getOrderInfo()");
+
+        int orderInfo = ThreadLocalRandom.current().nextInt(100);
+        log.info("Mock info generated: " + orderInfo);
+
+        int userInfo = svc.getUserInfo();
+        log.info("Mock user info fetched: " + userInfo);
+
+        return orderInfo + userInfo;
+    }
+}
